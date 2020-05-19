f37dc1dd9b7658484aca0f751945e49f3e59dc1a
Ahmet Selim ÇİL <hi@ahmetsc.com>
2020-05-19 15:09:29 +0300

    nav error

--- src/components/nav.js
+++ src/components/nav.js
@@ -10,168 +10,168 @@ export const items = [
 
   {
     title: i18n.t('nav.AuthorityIndex'),
-    router: 'AuthorityIndex',
+    router: 'Authority',
     icon: '',
     params: {},
     sub: []
   },    
   {
     title: i18n.t('nav.CourseIndex'),
-    router: 'CourseIndex',
+    router: 'Course',
     icon: '',
     params: {},
     sub: []
   },    
   {
     title: i18n.t('nav.CourseOutcomeIndex'),
-    router: 'CourseOutcomeIndex',
+    router: 'CourseOutcome',
     icon: '',
     params: {},
     sub: []
   },    
   {
     title: i18n.t('nav.AssessmentIndex'),
-    router: 'AssessmentIndex',
+    router: 'Assessment',
     icon: '',
     params: {},
     sub: []
   },    
   {
     title: i18n.t('nav.DepartmentIndex'),
-    router: 'DepartmentIndex',
+    router: 'Department',
     icon: '',
     params: {},
     sub: []
   },    
   {
     title: i18n.t('nav.DepartmentsHasInstructorsIndex'),
-    router: 'DepartmentsHasInstructorsIndex',
+    router: 'DepartmentsHasInstructors',
     icon: '',
     params: {},
     sub: []
   },    
   {
     title: i18n.t('nav.FacultyIndex'),
-    router: 'FacultyIndex',
+    router: 'Faculty',
     icon: '',
     params: {},
     sub: []
   },    
   {
     title: i18n.t('nav.GradingToolIndex'),
-    router: 'GradingToolIndex',
+    router: 'GradingTool',
     icon: '',
     params: {},
     sub: []
   },    
   {
     title: i18n.t('nav.GtccoIndex'),
-    router: 'GtccoIndex',
+    router: 'Gtcco',
     icon: '',
     params: {},
     sub: []
   },    
   {
     title: i18n.t('nav.IgsIndex'),
-    router: 'IgsIndex',
+    router: 'Igs',
     icon: '',
     params: {},
     sub: []
   },    
   {
     title: i18n.t('nav.LogIndex'),
-    router: 'LogIndex',
+    router: 'Log',
     icon: '',
     params: {},
     sub: []
   },    
   {
     title: i18n.t('nav.ProgramOutcomeIndex'),
-    router: 'ProgramOutcomeIndex',
+    router: 'ProgramOutcome',
     icon: '',
     params: {},
     sub: []
   },    
   {
     title: i18n.t('nav.PopcoIndex'),
-    router: 'PopcoIndex',
+    router: 'Popco',
     icon: '',
     params: {},
     sub: []
   },    
   {
     title: i18n.t('nav.SectionIndex'),
-    router: 'SectionIndex',
+    router: 'Section',
     icon: '',
     params: {},
     sub: []
   },    
   {
     title: i18n.t('nav.SagtIndex'),
-    router: 'SagtIndex',
+    router: 'Sagt',
     icon: '',
     params: {},
     sub: []
   },    
   {
     title: i18n.t('nav.SgmgcoIndex'),
-    router: 'SgmgcoIndex',
+    router: 'Sgmgco',
     icon: '',
     params: {},
     sub: []
   },    
   {
     title: i18n.t('nav.SgmgpoIndex'),
-    router: 'SgmgpoIndex',
+    router: 'Sgmgpo',
     icon: '',
     params: {},
     sub: []
   },    
   {
     title: i18n.t('nav.StsIndex'),
-    router: 'StsIndex',
+    router: 'Sts',
     icon: '',
     params: {},
     sub: []
   },    
   {
     title: i18n.t('nav.UniversityIndex'),
-    router: 'UniversityIndex',
+    router: 'University',
     icon: '',
     params: {},
     sub: []
   },    
   {
     title: i18n.t('nav.UserIndex'),
-    router: 'UserIndex',
+    router: 'User',
     icon: '',
     params: {},
     sub: []
   },    
   {
     title: i18n.t('nav.UsersAdminIndex'),
-    router: 'UsersAdminIndex',
+    router: 'UsersAdmin',
     icon: '',
     params: {},
     sub: []
   },    
   {
     title: i18n.t('nav.UsersStudentIndex'),
-    router: 'UsersStudentIndex',
+    router: 'UsersStudent',
     icon: '',
     params: {},
     sub: []
   },    
   {
     title: i18n.t('nav.UsersInstructorIndex'),
-    router: 'UsersInstructorIndex',
+    router: 'UsersInstructor',
     icon: '',
     params: {},
     sub: []
   },    
   {
     title: i18n.t('nav.CheckAuthIndex'),
-    router: 'CheckAuthIndex',
+    router: 'CheckAuth',
     icon: '',
     params: {},
     sub: []
